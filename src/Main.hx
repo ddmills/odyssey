2ca3a7859b6e8019165ff162a5533766a7a453ac
Dalton Mills <mills.dalton@gmail.com>
2022-08-26 20:36:06 -0500

    haxe+heaps

---
+++ src/Main.hx
@@ -0,0 +1,15 @@
+class Main extends hxd.App
+{
+	static function main()
+	{
+		hxd.Res.initEmbed();
+		new Main();
+	}
+
+	override function init()
+	{
+		hxd.Window.getInstance().title = "Odyssey";
+	}
+
+	override function update(dt:Float) {}
+}
